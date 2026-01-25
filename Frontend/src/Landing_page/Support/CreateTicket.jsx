2ef19b9690e49efd6013a5208629c6898ac91433
Thushar shetty <shettythushar31@gmail.com>
2026-01-25 12:18:32 +0530

    landing

--- Frontend/src/Landing_page/Support/CreateTicket.jsx
+++ Frontend/src/Landing_page/Support/CreateTicket.jsx
@@ -1,7 +1,84 @@
 function CreateTicket() {
-    return ( <div>
-        <h1>Create Ticket</h1>
-    </div> );
+    return (
+        <div className="container">
+            <div className="row p-5">
+                <h1 className="fs-3 text-center p-5 fw-normal">
+                    To create a ticket, select a relevant topic
+                </h1>
+                <div className="col-4 p-5">
+                    <h4 className="mb-5"> <i class="fa-solid fa-user-plus fa-2xs"></i> Account opening</h4>
+                    <div className="d-flex flex-column justify-content-evenly">
+                        <a href="" style={{ textDecoration: "none" }}>Online Account Opening</a><br />
+                        <a href="" style={{ textDecoration: "none" }}>Offline Account Opening</a><br />
+                        <a href="" style={{ textDecoration: "none" }}>Company, Partnership and HUF Account Opening</a><br />
+                        <a href="" style={{ textDecoration: "none" }}>NRI Account Opening</a><br />
+                        <a href="" style={{ textDecoration: "none" }}>Charges at Zerodha</a><br />
+                        <a href="" style={{ textDecoration: "none" }}>Zerodha IDFC FIRST Bank 3-in-1 Account</a><br />
+                        <a href="" style={{ textDecoration: "none" }}>Getting Started</a><br />
+                    </div>
+                </div>
+                <div className="col-4 p-5 ">
+                    <h4 className="mb-5"> <i class="fa-solid fa-user fa-2xs"></i> Your Zerodha Account</h4>
+                    <div className="d-flex flex-column justify-content-evenly">
+                        <a href="" style={{ textDecoration: "none" }}>Login Credentials</a><br />
+                        <a href="" style={{ textDecoration: "none" }}>Account Modification and Segment Addition</a><br />
+                        <a href="" style={{ textDecoration: "none" }}>DP ID and bank details</a><br />
+                        <a href="" style={{ textDecoration: "none" }}>Your Profile</a><br />
+                        <a href="" style={{ textDecoration: "none" }}>Transfer and conversion of shares</a><br />
+                    </div>
+                </div>
+                <div className="col-4 p-5 ">
+                    <h4 className="mb-5"> <i class="fa-solid fa-chart-column fa-2xs"></i> Your Zerodha Account</h4>
+                    <div className="d-flex flex-column justify-content-evenly">
+                        <a href="" style={{ textDecoration: "none" }}>Margin/leverage, Product and Order types</a><br />
+                        <a href="" style={{ textDecoration: "none" }}>Kite Web and Mobile</a><br />
+                        <a href="" style={{ textDecoration: "none" }}>Trading FAQs</a><br />
+                        <a href="" style={{ textDecoration: "none" }}>Corporate Actions</a><br />
+                        <a href="" style={{ textDecoration: "none" }}>Sentinel</a><br />
+                        <a href="" style={{ textDecoration: "none" }}>Kite API</a><br />
+                        <a href="" style={{ textDecoration: "none" }}>Pi and other platform</a><br />
+                        <a href="" style={{ textDecoration: "none" }}>Stockreports+</a><br />
+                        <a href="" style={{ textDecoration: "none" }}>GTT</a><br />
+                    </div>
+                </div>
+            </div>
+            <div className="row p-5">
+                <div className="col-4 px-5 mb-5">
+                    <h4 className="mb-5"> <i class="fa-solid fa-wallet fa-2xs"></i>Funds</h4>
+                    <div className="d-flex flex-column justify-content-evenly">
+                        <a href="" style={{ textDecoration: "none" }}>Adding Funds</a><br />
+                        <a href="" style={{ textDecoration: "none" }}>Fund Withdrawal</a><br />
+                        <a href="" style={{ textDecoration: "none" }}>eMandates</a><br />
+                        <a href="" style={{ textDecoration: "none" }}>Adding Bank Accounts</a><br />
+                    </div>
+                </div>
+                <div className="col-4 px-5 mb-5">
+                    <h4 className="mb-5"><i class="fa-solid fa-circle fa-2xs"></i> Console</h4>
+                    <div className="d-flex flex-column justify-content-evenly">
+                        <a href="" style={{ textDecoration: "none" }}>Reports</a><br />
+                        <a href="" style={{ textDecoration: "none" }}>Ledger</a><br />
+                        <a href="" style={{ textDecoration: "none" }}>Portfolio</a><br />
+                        <a href="" style={{ textDecoration: "none" }}>60 Day Challenge</a><br />
+                        <a href="" style={{ textDecoration: "none" }}>IPO</a><br />
+                        <a href="" style={{ textDecoration: "none" }}>Referral Program</a><br />
+                    </div>
+                </div>
+                <div className="col-4 px-5 mb-5">
+                    <h4 className="mb-5"> <i class="fa-solid fa-coins fa-2xs"></i>Coin</h4>
+                    <div className="d-flex flex-column justify-content-evenly">
+                        <a href="" style={{ textDecoration: "none" }}>Understanding Mutual Funds</a><br />
+                        <a href="" style={{ textDecoration: "none" }}>About Coin</a><br />
+                        <a href="" style={{ textDecoration: "none" }}>Buying and Selling through Coin</a><br />
+                        <a href="" style={{ textDecoration: "none" }}>Starting an SIP</a><br />
+                        <a href="" style={{ textDecoration: "none" }}>Managing your Portfolio</a><br />
+                        <a href="" style={{ textDecoration: "none" }}>Coin App</a><br />
+                        <a href="" style={{ textDecoration: "none" }}>Moving to Coin</a><br />
+                        <a href="" style={{ textDecoration: "none" }}>Government Securities</a><br />
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default CreateTicket;
--- Frontend/src/Landing_page/Support/Hero.jsx
+++ Frontend/src/Landing_page/Support/Hero.jsx
@@ -1,7 +1,32 @@
 function Hero() {
-    return ( <div>
-        <h1>Hero</h1>
-    </div> );
+    return ( 
+       <div className="text-white " style={{backgroundColor:"rgb(56,126,209)"}}>
+        <div className="container mt-5">
+            <div className="mt-5 d-flex justify-content-between" style={{margin:"0 150px"}}>
+                    <h5 className="mt-5">Support portal</h5>
+                    <a href="" className="mt-5" style={{color:"white"}}>Track tickets</a>
+            </div>
+            <div className="row  d-flex justify-content-between" style={{margin:"0 100px"}}>
+                <div className="col-7 p-5 mt-5">
+                    <h4>Search for an answer or browse help topics to create a ticket</h4>
+                    <input type="text" placeholder="Eg: how do i activate F&O, why is my order getting rejected.." 
+                       className="px-5 py-2 form-control"/><br />
+                    <a href="" style={{color:"white"}} className="mx-1">Track account opening</a>
+                    <a href="" style={{color:"white"}} className="mx-1">Track segment activation</a>
+                    <a href="" style={{color:"white"}} className="mx-1">Intraday margins</a>
+                    <a href="" style={{color:"white"}} className="mx-1">Kite user manual</a>
+                </div>
+                <div className="col-5 py-5 ps-5 mt-5">
+                    <h4>Featured</h4>
+                    <ol>
+                        <li><a href="" style={{color:"white"}} className="lh-lg">Current Takeovers and Delisting - January 2024</a></li>
+                        <li><a href="" style={{color:"white"}} className="lh-lg">Latest Intraday leverages - MIS & CO</a></li>
+                    </ol>
+                </div>
+            </div>
+        </div>
+       </div>
+    );
 }
 
 export default Hero;
